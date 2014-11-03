d80781ea3bd2c38455e70f7bbf8846f24ca0b30c
胡劼 <xsir317@gmail.com>
2014-11-03 17:00:42 +0800

    增加数据库处理
    medoo数据库框架，以及修改的一堆东西

---
+++ wwwroot/application/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 所有在Bootstrap类中, 以_init开头的方法, 都会被Yaf调用,
+ * 这些方法, 都接受一个参数:Yaf_Dispatcher $dispatcher
+ * 调用的次序, 和申明的次序相同
+ */
+class Bootstrap extends Yaf_Bootstrap_Abstract{
+
+        public function _initLocalLibrary() {
+     		Yaf_Loader::getInstance()->registerLocalNamespace(array("Db"));
+        }
+}
+    
