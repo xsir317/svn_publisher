7396dc759a8db0099db3778a672e45ced63faff1
胡劼 <xsir317@gmail.com>
2014-11-11 14:46:23 +0800

    很萌的favicon（GITHUB的），以及TaskHelper的架子

--- publisher/app/lib/TaskHelper.php
+++ publisher/app/lib/TaskHelper.php
@@ -6,8 +6,57 @@
 namespace TaskCore;
 class TaskHelper 
 {
-    public function work()
+	/**
+	 * 
+	 * 针对一个任务，调用适当方法进行处理，处理后回写执行结果
+	 * 任务种类 'checkout', 'update', 'delete', 'rsync'
+	 * 
+	*/
+    public function run($task)
     {
-        return "it works!";
+    	//TODO 检查状态 'created', 'execute', 'success', 'failed'
+    	//TODO 检查前置任务pre_task 的状态
+        $func = '_run'.ucfirst($task->type);
+        return $this->$func($task);
     }
+
+    /**
+     * 针对一个项目Project，获取最新版本库，放到指定目录，并将版本号回写到Project记录中。
+     * 
+    */
+    private function _runCheckout($task)
+    {
+
+    }
+
+    /**
+     * 针对一个项目，将其发布代码目录更新到指定版本，并将版本号回写到Project记录中。
+     * 
+    */
+    private function _runUpdate($task)
+    {
+
+    }
+
+
+    /**
+     * 针对一个项目，将其发布代码目录清空，并将Project记录中的版本号置空。
+     * 
+    */
+    private function _runDelete($task)
+    {
+
+    }
+
+    /**
+     * 
+     * 针对一个服务器，调用Rsync将项目
+     * 
+    */
+    private function _runRsync($task)
+    {
+
+    }
+
+
 }
