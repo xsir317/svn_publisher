58054fcacb5b481a96223c7e5e16b558329ec5f2
胡劼 <xsir317@gmail.com>
2014-11-27 17:54:59 +0800

    删除以前的YAF相关内容

--- wwwroot/application/Bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Bootstrap extends Yaf_Bootstrap_Abstract{
-	public function _initLocalLibrary() 
-	{
-		Yaf_Loader::getInstance()->registerLocalNamespace(array("Db"));
-	}
-}
--- wwwroot/application/controllers/Index.php
+++
@@ -1,6 +0,0 @@
-<?php
-class IndexController extends Yaf_Controller_Abstract {
-   public function indexAction() {//默认Action
-       $this->_view->assign("content", "Hello World");
-   }
-}
